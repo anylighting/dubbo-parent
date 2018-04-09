a48a17c9b22583641b05b8f449bbc30fca8898c4
zhaoji0596 <hezhenq@yeah.net>
2018-04-09 23:54:37 +0800

    使用zookeeper配置中心

--- dubbo-consumer/src/main/java/com/zhaoji/dubboconsumer/AppClass.java
+++ dubbo-consumer/src/main/java/com/zhaoji/dubboconsumer/AppClass.java
@@ -1,5 +1,7 @@
 package com.zhaoji.dubboconsumer;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -10,15 +12,17 @@ public class AppClass {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(AppClass.class);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		context.start();
-		
+		System.out.println("客户端启动...................");
 		DemoService demoService = context.getBean(DemoService.class);
 		String result = demoService.sayHello("zhaoji");
 		System.out.println("客户端收到结果" + result);
 		LOGGER.info("客户端输出：{}", result);
+		LOGGER.info("客户段启动。。。。。。。。。。。。。。");
+		System.in.read();
 		context.close();
 		context.stop();
 	}
